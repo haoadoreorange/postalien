65cb2c5130be1153e0a4a4a707d683ebb02cc018
haoadoresorange <github@comtam.dev>
2021-11-02 12:21:42 -0400

    gitpkg

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import "lib/polyfill";
+import { Requests } from "types/requests";
+export declare const postwoman: (rqs: Requests) => void;
+export { Requests } from "types/requests";
+export * from "lib/varnager";
---
+++ dist/lib/polyfill.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),String.prototype.replaceAll||(String.prototype.replaceAll=function(e,t){return"[object regexp]"===Object.prototype.toString.call(e).toLowerCase()?this.replace(e,t):this.replace(new RegExp(e,"g"),t)});
+//# sourceMappingURL=polyfill.js.map
